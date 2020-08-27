1ba7483a4205e06605cce148aa1cff2d1a9c2554
j-quinto <japioquinto@gmail.com>
2020-08-26 22:11:03 -0400

    Update Before Addition of new Conditios
    Finalized the magnetic fields for AC and DC fields.

--- a_n_Simulation/a_n_Simulation/a_n_functions.h
+++ a_n_Simulation/a_n_Simulation/a_n_functions.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <Eigen/Dense>
+#include <string>
 
 using namespace std;
 using namespace Eigen;
@@ -14,17 +15,31 @@ const double pi = 3.1415926535897;
 const double h = 1.054571817e-34; //Js hbar
 const double m_n = 1.67492749805e-27; //kg neutron mass
 const double g = 9.81; //m/s^2 gravitational constant
-const double zo = pow(pow(h, 2) / (2.0 * g * pow(m_n, 2)), 1.0/3.0);
+const double zo = pow(pow(h,2)/(2.0*g*pow(m_n, 2)),1.0/3.0);
 const double gam_n = 183e6;
-const double mu_n = gam_n * h / (2); //neutron magnetic moment
-//const double mu_n = gam_n * h / (4 * pi); //neutron magnetic moment
-const double v = 4.91; //m/s
-const double d = 1e-3; //m
-//const double w = 2 * pi * v / d; //rad/s
-const double w = 2660.0; //rad/s
+const double mu_n = -1.0*gam_n*h/2; //neutron magnetic moment
+const double v = 5.5; //m/s
+const double L = 0.16; //m/s
+const double d = 1e-2; //m
+const double f = 130; //Hz
+const double w = 2*pi*v/d; //rad/s
+const double wd = 2*pi*f;
+const double p = 4*pi/4;
+const double Ia = 1.4; //A
+const double Ib = 3.5; //A
+const double mu = 4*pi*1e-7;
+const double dx = 1e-3; //mm wire cross section length
+const double dz = 1e-3; //mm wire cross section height
+const double Dz = 1.3e-3; //mm distance between center of wire to the mirror
+const double k = -(mu*d/(pow(pi,2)*dx*dz))*exp(-2*pi*Dz/d)*sinh(pi*dz/d)*sin(pi*dx/d);
+//const double w = 2910.0 - 200.0; //rad/s
 double dt;
 double TF;
 double t;
+double t_2;
+double t_3;
+double Bx;
+double Bz;
 int m;
 int l;
 
@@ -39,24 +54,42 @@ complex<double> HBmp;
 complex<double> w_ml;
 
 
-const int N = 4; //number of states/size of a
+const int N = 8; //number of states/size of a
 ArrayXcd k1(N), k2(N), k3(N), k4(N);
 ArrayXd A(N);
 ArrayXd n(N); //state vector
 ArrayXd z(N); //m state heights 
 ArrayXd E(N); //J state energies
 ArrayXcd a(N); //coefficient vect
-ArrayXcd dadt(N, 1);
-ArrayXXd b(3, 3);
-ArrayXXd Bo(3, 3); //time dependent magnetic field coefficients
+ArrayXcd dadt(N,1);
+ArrayXXd b(3,3);
+ArrayXXd Bo(3,3); //time dependent magnetic field coefficients
 ArrayXd Bh(3); //holding magnetic field
 
-ArrayXXd B(double t, ArrayXd Bh, ArrayXXd Bo)
+const char* mode = "AC";
+
+ArrayXXd B(double t, ArrayXd Bh, ArrayXXd Bo, const char* mode)
 {
-    b << Bh(0) - Bo(0, 0) * sin(w * t), Bh(1) + Bo(0, 1), Bh(2) + Bo(0, 2) * cos(w * t),
-                -Bo(1, 0) * sin(w * t),         Bo(1, 1),          Bo(1, 2)* cos(w * t),
-                 -Bo(2, 0) * sin(w * t),        Bo(2, 1),          Bo(2, 2)* cos(w * t);
-    return b;
+    if (strcmp(mode,"DC") == 0) {
+        b << Bh(0) + Bo(0,0)*sin(w*t), Bh(1) + Bo(0,1), Bh(2) + Bo(0,2)*cos(w*t),
+                     Bo(1,0)*sin(w*t),         Bo(1,1),         Bo(1,2)*cos(w*t),
+                     Bo(2,0)*sin(w*t),         Bo(2,1),         Bo(2,2)*cos(w*t);
+        return b;
+    }
+    else if (strcmp(mode, "AC") == 0) {
+        Bx = k*cos(wd*t + p)*(Ia*((sqrt(2) - 2)*cos(w*t) + sqrt(2)*sin(w*t)) + Ib*((sqrt(2) + 2)*sin(w*t) - sqrt(2)*cos(w*t)));
+        Bz = k*cos(wd*t + p)*(Ia*((sqrt(2) - 2)*sin(w*t) - sqrt(2)*cos(w*t)) - Ib*((sqrt(2) + 2)*cos(w*t) + sqrt(2)*sin(w*t)));
+        b <<      Bh(0) + Bx, Bh(1),       Bh(2) + Bz,
+                 (2*pi/d)*Bx,   0.0,      (2*pi/d)*Bz,
+            pow(2*pi/d,2)*Bx,   0.0, pow(2*pi/d,2)*Bz;
+        return b;
+    }
+    else{
+        b << 0, 0, 0,
+             0, 0, 0,
+             0, 0, 0;
+        return b;
+    }
 }
 
 ArrayXcd da_dt(double t, ArrayXcd a, ArrayXXd B)
@@ -68,7 +101,7 @@ ArrayXcd da_dt(double t, ArrayXcd a, ArrayXXd B)
             for (l = 0; l < N; l = l + 2) {
                 if (l == m) {
                     HBpp = -(B(0, 2) + B(1, 2) * (2.0 / 3.0) * z(m) + B(2, 2) * (4.0 / 15.0) * pow(z(m), 2));
-                    HBpm = -(B(0, 0) + B(1, 0) * (2.0 / 3.0) * z(m) + B(2, 0) * (4.0 / 15.0) * pow(z(m), 2));
+                    HBpm = -(B(0, 0) - B(0, 1) * 1.0i + B(1, 0) * (2.0 / 3.0) * z(m) + B(2, 0) * (4.0 / 15.0) * pow(z(m), 2));
                 }
                 else {
                     HBpp = -(B(1, 2) * 2.0 * pow(-1, n(m) - n(l) - 1) * pow(zo, 3) / pow(z(m) - z(l), 2) +
@@ -87,7 +120,7 @@ ArrayXcd da_dt(double t, ArrayXcd a, ArrayXXd B)
             for (l = 1; l < N; l = l + 2) {
                 if (l == m) {
                     HBmm = (B(0, 2) + B(1, 2) * (2.0 / 3.0) * z(m) + B(2, 2) * (4.0 / 15) * pow(z(m), 2));
-                    HBmp = -(B(0, 0) + B(1, 0) * (2.0 / 3.0) * z(m) + B(2, 0) * (4.0 / 15.0) * pow(z(m), 2));
+                    HBmp = -(B(0, 0) + B(0, 1) * 1.0i + B(1, 0) * (2.0 / 3.0) * z(m) + B(2, 0) * (4.0 / 15.0) * pow(z(m), 2));
                 }
                 else {
                     HBmm = (B(1, 2) * 2.0 * pow(-1, n(m) - n(l) - 1) * pow(zo, 3) / pow(z(m) - z(l), 2) +
--- a_n_plotter.m
+++ a_n_plotter.m
@@ -1,17 +1,103 @@
-%A = importdata('C:\Users\japio\Documents\GitHub\constantB.csv');
-%A = importdata('C:\Users\japio\Documents\GitHub\B_z.csv');
-A = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z.csv');
+clear
+A = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z_AC_n1234_f130_v5.5_ppi1.csv');
+
+%{
+A = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z_15G_n13_w-0.csv');
+B = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z_15G_n13_w-100.csv');
+C = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z_15G_n13_w-200.csv');
+D = importdata('C:\Users\japio\Documents\GitHub\GRANIT-Simulation\a_n_Simulation\a_n_Simulation\B_z_15G_n13_w-250.csv');
+
+a = A.data;
+b = B.data;
+%c = C.data;
+%d = D.data;
+ta = a(:,1);
+a1a_up = a(:,2);
+a1a_down = a(:,3);
+a1a = sum(a(:,2:3),2);
+tb = b(:,1);
+a1b_up = b(:,2);
+a1b_down = b(:,3);
+a1b = sum(b(:,2:3),2);
+
+tc = c(:,1);
+a1c_up = c(:,2);
+a1c_down = c(:,3);
+a1c = sum(c(:,2:3),2);
+td = d(:,1);
+a1d_up = d(:,2);
+a1d_down = d(:,3);
+a1d = sum(d(:,2:3),2);
+%}
+
 d = A.data;
 t = d(:,1);
 a1_up = d(:,2);
 a1_down = d(:,3);
+%{
 a3_up = d(:,4);
 a3_down = d(:,5);
+Tot = sum(d(:,2:5),2);
 a1 = a1_up + a1_down;
 a3 = a3_up + a3_down;
-Tot = sum(d(:,2:5)');
+%}
+a2_up = d(:,4);
+a2_down = d(:,5);
+a3_up = d(:,6);
+a3_down = d(:,7);
+a4_up = d(:,8);
+a4_down = d(:,9);
+%a5_up = d(:,10);
+%a5_down = d(:,11);
+Tot = sum(d(:,2:9),2);
+a1 = a1_up + a1_down;
+a2 = a2_up + a2_down;
+a3 = a3_up + a3_down;
+a4 = a4_up + a4_down;
+%a5 = a5_up + a5_down;
+
 %plot(t,a1_up,t,a1_down,t,a3_up,t,a3_down); legend('a1_u_p','a1_d_o_w_n','a3_u_p','a3_d_o_w_n','Total');
-plot(t, a1, t, a3, t, Tot); legend('a1','a3','Total');
-%plot(t, a1); legend('a1');
-xlabel('time (s)')
-ylabel('state population')
+%plot(t, a1, t, a2, t, a3, t, a4, t, a5, t, Tot); legend('a1','a2','a3','a4','a5','Total');
+%plot(t, a1, t, a3, t, Tot); legend('a1','a3','Total');
+%plot(t, a1, t, Tot); legend('a_1','Total');
+
+L = 0.16; 
+v = 4.00;
+to = L/v;
+%N = interp1(t, a1, to);
+N = a1(end)
+
+fa = 60.0; Na = 0.0203;
+f0 = 70.0; N0 = 0.0589; 
+f1 = 85.0; N1 = 0.0326; 
+f2 = 100.0; N2 = 0.1730; 
+f3 = 105.0; N3 = 0.010903811900000;
+f4 = 113.5; N4 = 0.4907; 
+f5 = 130.0; N5 = 0.0742; 
+f6 = 141.5; N6 = 0.5097; 
+f7 = 160.0; N7 = 0.0321; 
+%{
+plot(fa,Na,'x',f0,N0,'x',f1,N1,'x',f2,N2,'x',f3,N3,'x',f4,N4,'x',f5,N5,...
+    'x',f6,N6,'x',f7,N7,'x',f6,N,'o');
+%plot([fa,f0,f1,f2,f3,f4,f5,f6],[Na,N0,N1,N2,N3,N4,N5,N6]);
+xlim([0.0,300]);
+ylim([0,0.7]);
+xlabel('driving frequency (Hz)')
+ylabel('ground state population')
+%}
+%{
+plot(t, a1); legend('a_1^2');
+hold on
+plot(to, No, "x"); legend('a_1^2: t_0 = L/v');
+ylim([0,1]);
+%}
+%{
+plot(t, a1, t, a2, t, Tot); legend('a1','a2','Total');
+hold on
+plot(to, No, "x"); legend('a_1^2: t_0 = L/v');
+%}
+%plot(ta, a1a, tb, a1b, tc, a1c, td, a1d); legend('w = 2910 rad/s','w-100 rad/s','w-200 rad/s','w-250 rad/s');
+%plot(ta, a1a, tb, a1b); legend('n = 3 spin down: w-250','n = 3 spin up: w+250');
+%plot(ta, a1a, tb, a1b, tc, a1c); legend('B_0 = 15G','B_0 = 50G','B_0 = 200G');
+%xlabel('time (s)')
+%ylabel('ground state population')
